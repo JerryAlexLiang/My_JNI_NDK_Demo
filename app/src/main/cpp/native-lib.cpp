0c51379bd1a8eb32c0c19b9d2c8a7780e089ac65
1935990239@qq.com <11935990239Smile>
2019-03-27 11:02:49 +0800

    关联native

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,6 +10,6 @@ JNIEXPORT jstring
 JNICALL
 Java_liang_com_my_1jni_1ndk_1demo_NDKTools_getStringFromNDK(
         JNIEnv *env, jclass type) {
-    std::string hello = "(*^__^*) Hello from C++ My First JNI NDK Demo";
+    std::string hello = "(*^__^*) Java2JNI C++ My First JNI NDK Demo";
     return env->NewStringUTF(hello.c_str());
 }
